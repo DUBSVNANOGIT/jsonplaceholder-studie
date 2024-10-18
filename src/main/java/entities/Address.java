43fd88dcfb05b86c7d11cf14270f6adc715bf83d
DUBSVANANOGIT <arthur.bic_@hotmail.com>
2024-10-18 19:39:28 -0300

    User, Company, Address created

---
+++ src/main/java/entities/Address.java
@@ -0,0 +1,68 @@
+package entities;
+
+public class Address {
+    private String street;
+    private String suite;
+    private String city;
+    private String zipcode;
+    private double lat;
+    private double lnt;
+
+    public Address(String street, String suite, String city, String zipcode, double lat, double lnt) {
+        this.street = street;
+        this.suite = suite;
+        this.city = city;
+        this.zipcode = zipcode;
+        this.lat = lat;
+        this.lnt = lnt;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getSuite() {
+        return suite;
+    }
+
+    public void setSuite(String suite) {
+        this.suite = suite;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLnt() {
+        return lnt;
+    }
+
+    public void setLnt(double lnt) {
+        this.lnt = lnt;
+    }
+}
+
---
+++ src/main/java/entities/User.java
@@ -0,0 +1,55 @@
+package entities;
+
+public class User {
+    private long id;
+    private String name;
+    private String username;
+    private String email;
+    private Address address;
+    private Company company;
+
+    public User(long id, String name,
+            String username, String email, String street, String suite, String city, String zipcode, double lat, double lnt, String name2, String catchPhrase, String bs) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.address = new Address(street, suite, city, zipcode, lat, lnt);
+        this.company = new Company(name2, catchPhrase, bs);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+    return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+    return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+}
+
+
