dab5da31d8d7ddb7e2293e8c71266d42fffe81fe
DUBSVANANOGIT <arthur.bic_@hotmail.com>
2024-10-18 15:23:46 -0300

    Start

---
+++ src/main/java/arthur/jsonplaceholder_studie/JsonplaceholderStudieApplication.java
@@ -0,0 +1,13 @@
+package arthur.jsonplaceholder_studie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonplaceholderStudieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonplaceholderStudieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arthur/jsonplaceholder_studie/JsonplaceholderStudieApplicationTests.java
@@ -0,0 +1,13 @@
+package arthur.jsonplaceholder_studie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonplaceholderStudieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
